bae9df9fbb16225b46f76fd5dc8f733c0bdea8ad
BILLET Thomas <b19017078@L-024110A006-08.salsa.univ-amu.fr>
2020-02-21 15:19:01 +0100

    fix rot13 and Cesar

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -1,13 +1,14 @@
 #include "ROT13.h"
+#include "Cesar.h"
 
 #include <stddef.h>
 
 char* chiffre_ROT13(char* clair)
 {
-    return NULL;
+    return chiffre_Cesar(clair, 'a'+13 );
 }
 
 char* dechiffre_ROT13(char* chiffre)
 {
-    return NULL;
+    return dechiffre_Cesar(chiffre, 'a'+13);
 }
