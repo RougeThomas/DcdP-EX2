47e5e805cc189ba3cf61bf644455d0eb0a5a9d62
BILLET Thomas <b19017078@L-024110A006-08.salsa.univ-amu.fr>
2020-02-21 14:38:06 +0100

    chiffrement cesar

--- lib/Cesar.c
+++ lib/Cesar.c
@@ -4,10 +4,21 @@
 
 char* chiffre_Cesar(char* clair, char const cle)
 {
-    return NULL;
+    int i = 0;
+    char decalage = cle - 'a';
+
+    while(clair[i] != '\0'){
+        if((clair[i] >= 'a' && clair[i] <= 'z') || (clair[i] >= 'A' && clair[i] <= 'Z')){
+            clair[i] = (clair[i] - 'a' + decalage)%26 + 'a';
+        }
+        i++;
+    }
+
+    return clair;
 }
 
 char* dechiffre_Cesar(char* chiffre, char const cle)
 {
+    
     return NULL;
 }
